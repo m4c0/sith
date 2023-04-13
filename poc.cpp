594c6c2030fa7a64e57bb45517097d90355bb9e7
Eduardo Costa <m4c0@users.noreply.github.com>
2023-04-13 11:34:02 +0100

    basic concept

--- poc.cpp
+++ poc.cpp
@@ -1,3 +1,35 @@
 import silog;
 
-int main() { silog::log(silog::info, "OK"); }
+namespace sith {
+class thread {
+  volatile bool m_interrupted{};
+
+  void interrupt() noexcept;
+
+protected:
+  [[nodiscard]] bool interrupted() const noexcept { return m_interrupted; }
+
+  virtual void run() = 0;
+
+public:
+  thread();
+  virtual ~thread() noexcept {
+    m_interrupted = true;
+    interrupt();
+  }
+};
+} // namespace sith
+
+class thread : public sith::thread {
+  void run() override {
+    while (!interrupted()) {
+      silog::log(silog::info, "Thread is waiting");
+    }
+  }
+};
+
+int main() {
+  thread t1{};
+  thread t2{};
+  silog::log(silog::info, "Threads started");
+}
