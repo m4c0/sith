ab55bb63649df59baaf46ad30b13f8c6d9fd33c4
Eduardo Costa <m4c0@users.noreply.github.com>
2023-04-13 13:44:46 +0100

    fix windows build

--- windows.cpp
+++ windows.cpp
@@ -1,10 +1,21 @@
 #include <windows.h>
 
+struct wrap {
+  void (*fn)(void *);
+  void *data;
+};
+DWORD void_start_wrap(void *wrapped) {
+  wrap *w = static_cast<wrap *>(wrapped);
+  w->fn(w->data);
+  delete w;
+  return 0;
+}
+
 extern "C" void *sith_create(void *data, void (*fn)(void *)) {
-  return CreateThread(nullptr, 0, fn, data,
+  return CreateThread(nullptr, 0, void_start_wrap, new wrap{fn, data},
                       0, // CREATE_SUSPENDED / ResumeThread
                       nullptr);
 }
 extern "C" void sith_destroy(void *nth) {
-  WaitForSingleObject((HANDLE)nth, INFINITY);
+  WaitForSingleObject((HANDLE)nth, INFINITE);
 }
